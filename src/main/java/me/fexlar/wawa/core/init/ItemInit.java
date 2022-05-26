bcef4dec3b569363a1e29481633c4f6cc6406470
fexlar <fexlarinvr@gmail.com>
2022-05-26 11:32:20 -0500

    Create ItemInit.java

---
+++ src/main/java/me/fexlar/wawa/core/init/ItemInit.java
@@ -0,0 +1,17 @@
+package me.fexlar.wawa.core.init;
+
+import me.fexlar.wawa.wawa;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public final class ItemInit {
+    private ItemInit() {}
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, wawa.MODID);
+
+    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+}
