c5b7d2182246c067f982d95673b9b313d6d9248b
fexlar <fexlarinvr@gmail.com>
2022-05-26 11:33:31 -0500

    Register ItemInit to Main

--- src/main/java/me/fexlar/wawa/wawa.java
+++ src/main/java/me/fexlar/wawa/wawa.java
@@ -1,12 +1,16 @@
 package me.fexlar.wawa;
 
+import me.fexlar.wawa.core.init.ItemInit;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(value = wawa.MODID)
 public class wawa {
     public static final String MODID = "wawa";
 
     public wawa() {
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ItemInit.ITEMS.register(bus);
     }
 }
