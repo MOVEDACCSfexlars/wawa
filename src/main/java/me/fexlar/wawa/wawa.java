6a03e2baa4db3e5a1d4f969791175421915de7b5
fexlar <fexlarinvr@gmail.com>
2022-05-26 11:20:03 -0500

    added java package n stuff ye

---
+++ src/main/java/me/fexlar/wawa/wawa.java
@@ -0,0 +1,12 @@
+package me.fexlar.wawa;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = wawa.MODID)
+public class wawa {
+    public static final String MODID = "wawa";
+
+    public wawa() {
+
+    }
+}
